07a2ff32b996c6a85afaec0932c743922209c268
opkiz <opkiznoya@gmail.com>
2021-02-03 04:02:36 +0900

    살려ㅈ ㅡㅑ

--- opkiz-main/public/js/front.js
+++ opkiz-main/public/js/front.js
@@ -1,88 +1,92 @@
 /********** items **********/
-var tourNow = 0, itemSize = 6; 
-var tours = [], itemArr = [];
-var tourLast;
-var tourLeft;
-var tourTar = 0;
-var root = $(".item-wrap");
-var tours = $(".items", root);
-
-$.get("../json/items.json", onitemsLoad);
-
-function onToursLoad(r) {
-	tourLast = r.tours.length - 1;
-	tourBars();
-	var html = '';
-	for (var i in r.items) {
-		html  = '<li class="items">';
-		html += '<div class="item-img">';
-		html += '<img src="' + r.items[i].src + '" alt="" class="img">';
-		html += '<div class="item-name">' + r.items[i].name + '</div>';
-		html += '</div>';
-		html += '</li>';
-		tours.push($(html).appendTo(".item-slide-wrap", root));
-	}
-	tourInit();
-}
-
-function tourBars() {
-	for (var i = 0; i <= tourLast; i++) {
-		$('<span class="bar"></span>').appendTo(".tour-slide-bar", root)
-	}
-}
+var itemNow = 0, itemSize = 6; 
+var items = [], itemArr = [];
+var itemLast;
+var itemLeft;
+var itemTar = 0;
+var root = $(".items-wrap");
+var items = $(".items", root);
 
 $(".bt-prev", root).click(onPrev);
 $(".bt-next", root).click(onNext);
 
 
-function tourInit() {
-	var trans = tourNow%2;
-	$(".tour-slide-wrap", root).css("left", "-20%");
-	$(".tours", root).remove();
-	tourArr = [];
-	tourArr[1] = tourNow;
-	tourArr[0] = (tourNow == 0) ? tourLast : tourNow - 1;
-	for (var i = 2; i < tourSize; i++) {
-		tourArr[i] = (tourArr[i - 1] == tourLast) ? 0 : tourArr[i - 1] + 1;
-	} 
-	for (var i in tourArr) {
-		$(tours[tourArr[i]]).clone().appendTo(".tour-slide-wrap", root);
+$.get("../json/item.json", onItemsLoad);
+
+function onItemsLoad(r) {
+	itemLast = r.items.length - 1;
+	itemBars();
+	var html = '';
+	for (var i in r.items) {
+		html  = '<li class="items">';
+		html += '<div class="item-img">';
+		html += '<img src="' + r.items[i].src + '" alt="item" class="img">';
+    html += '</div>';
+    html += '<div class="item-desc">';
+    html += '<div class="item-name">' + r.items[i].name + '</div>';
+    html += '<p>see more</p>';
+    html += '</div>'; 
+		html += '</li>';
+		items.push($(html).appendTo(".item-slide-wrap", root));
 	}
-	$(".tours").each(function(){
+	itemInit();
+}
+
+function itemBars() {
+	for (var i = 0; i <= itemLast; i++) {
+		$('<span class="bar"></span>').appendTo(".item-slide-bar", root)
+	}
+}
+
+
+function itemInit() {
+	var trans = itemNow%2;
+	$(".item-slide-wrap", root).css("left", "-20%");
+	$(".items", root).remove();
+	itemArr = [];
+	itemArr[1] = itemNow;
+	itemArr[0] = (itemNow == 0) ? itemLast : itemNow - 1;
+	for (var i = 2; i < itemSize; i++) {
+		itemArr[i] = (itemArr[i - 1] == itemLast) ? 0 : itemArr[i - 1] + 1;
+	} 
+	for (var i in itemArr) {
+		$(items[itemArr[i]]).clone().appendTo(".item-slide-wrap", root);
+	}
+	$(".items").each(function(){
 		if($(this).index()%2) $(this).css("transform", "translateY(50px)");
 		else $(this).css("transform", "none");
 	});
-	$(".tour-slide-bar .bar", root).removeClass("active");
-	$(".tour-slide-bar .bar", root).eq(tourNow).addClass("active");
+	$(".item-slide-bar .bar", root).removeClass("active");
+	$(".item-slide-bar .bar", root).eq(itemNow).addClass("active");
 }
 
 function onPrev() {
-	tourNow = (tourNow == 0) ? tourLast : tourNow - 1;
-	tourTar = 0;
-	tourAni();
+	itemNow = (itemNow == 0) ? itemLast : itemNow - 1;
+	itemTar = 0;
+	itemAni();
 }
 
 function onNext() {
-	tourNow = (tourNow == tourLast) ? 0 : tourNow + 1;
-	tourTar = "-40%";
-	tourAni();
+	itemNow = (itemNow == itemLast) ? 0 : itemNow + 1;
+	itemTar = "-40%";
+	itemAni();
 }
 
-console.log($(".tour-slide-bar .bar", root).click("hi"));
-$(".tour-slide-bar .bar", root).click(onTourBarClick);
-function onTourBarClick() {
-	$(".tour-slide-bar .bar", root).removeClass("active");
-	$(".tour-slide-bar .bar", root).eq(0).addClass("active");
-	var tourTar = [];
-	var old = tourNow;
-	tourNow = $(this).index();
-	tourTar = (tourNow > old) ? "-40%" : 0;
-	$(".tour-slide-wrap", root).stop().animate({"left": tourTar}, 500, tourInit);
-	tourAni();
+console.log($(".item-slide-bar .bar", root).click("hi"));
+$(".item-slide-bar .bar", root).click(onitemBarClick);
+function onItemBarClick() {
+	$(".item-slide-bar .bar", root).removeClass("active");
+	$(".item-slide-bar .bar", root).eq(0).addClass("active");
+	var itemTar = [];
+	var old = itemNow;
+	itemNow = $(this).index();
+	itemTar = (itemNow > old) ? "-40%" : 0;
+	$(".item-slide-wrap", root).stop().animate({"left": itemTar}, 500, itemInit);
+itemrAni();
 }
 
-function tourAni() {
-	$(".tours").each(function(){
+function itemAni() {
+	$(".items").each(function(){
 		if($(this).css("transform") == "none") {
 			$(this).css("transform", "translateY(50px)")
 		}
@@ -90,10 +94,21 @@ function tourAni() {
 			$(this).css("transform", "none");
 		}
 	});
-	$(".tour-slide-wrap", root).stop().animate({"left": tourTar}, 500, tourInit);
+	$(".item-slide-wrap", root).stop().animate({"left": itemTar}, 500, itemInit);
 }
 
 
 /********** 이벤트 등록 **********/
 $(window).resize(onResize).trigger("resize");
 $(window).scroll(onScroll).trigger("scroll");
+
+/******* 페이지이동*******/
+$(".items-wrap").find("p:nth-child(1)").click(function() {
+	location.href = "https://opkiz-bagel.web.app/html/index.html";
+});
+$(".items-wrap").find("p:nth-child(2)").click(function() {
+	location.href = "https://opkiz-kbp.web.app/html/index.html";
+});
+$(".items-wrap").find("p:nth-child(3)").click(function() {
+	location.href = "https://opkiz-shop.web.app/html/index.html";
+});
